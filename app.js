ebd7cc9648582db2d40789856e344bcebaac1f08
Luis Maracara <luigi.maracara@gmail.com>
2019-10-23 17:34:04 -0300

    cleaned files added commander and winston

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-const ens = require('./environment');
-
 // Express App Setup
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const helmet = require('helmet');
+const { logError, logRequest } = require('./services/loggin');
 // Routes
 const apiRoutes = require('./routes/api.routes');
+
+
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(helmet());
 
+app.use(logError);
+app.use(logRequest);
 app.use('/api', apiRoutes);
 
 app.listen(6512, err => {
-  console.log("Listening on port 6512");
+  console.log('Listening on port 6512');
 });
